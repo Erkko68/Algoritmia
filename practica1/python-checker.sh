1d40c4ad660524162e7fcda9b3548c632718ca54
Eric Bitria <eric.bitria@gmail.com>
2024-03-31 12:59:22 +0200

    added timers checks

--- practica1/python-checker.sh
+++ practica1/python-checker.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 directory="tests/"
-echo $directory
-for x in {2..4}
-do
-   for y in {0..9}
-   do
-      echo "##############"
-      inputfile=$directory'input'$x'-'$y'.txt'
-      echo Testing $inputfile
-      python3 gerarquia.py $inputfile > output.txt
-      outputfile=$directory'output'$x'-'$y'.txt'
-      diff output.txt $outputfile
-   done
+total_time=0
+num_runs=0
+
+for x in {2..4}; do
+    for y in {0..9}; do
+        echo "##############"
+        inputfile="$directory/input$x-$y.txt"
+        echo "Testing $inputfile"
+        
+        start=$(date +%s%N)
+        python3 gerarquia.py "$inputfile" > output.txt
+        endt=$(date +%s%N)
+        runtime=$((endt-start))
+        total_time=$((total_time + runtime))
+        num_runs=$((num_runs + 1))
+
+        echo "Execution time for run $num_runs: $((runtime)) ns"
+
+        outputfile="$directory/output$x-$y.txt"
+        diff output.txt "$outputfile"
+    done
 done
+
+average_time=$((total_time / num_runs))
+echo "Average execution time: $((average_time)) ns"
