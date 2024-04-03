b74be12641d463c7631c494b0b2bb530775c2e0a
Eric Bitria <eric.bitria@gmail.com>
2024-04-03 15:12:33 +0200

    removed timers

--- practica1/haskell-checker.sh
+++ practica1/haskell-checker.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 
 directory="tests/"
-total_time=0
-num_runs=0
-
-for x in {2..4}; do
-    for y in {0..9}; do
-        echo "##############"
-        inputfile="$directory/input$x-$y.txt"
-        echo "Testing $inputfile"
-
-        start=$(date +%s%N)
-        ./gerarquia "$inputfile" > output.txt
-        endt=$(date +%s%N)
-        runtime=$((endt-start))
-        total_time=$((total_time + runtime))
-        num_runs=$((num_runs + 1))
-
-        echo "Execution time for run $num_runs: $runtime ns"
-
-        outputfile="$directory/output$x-$y.txt"
-        diff output.txt "$outputfile"
-    done
+echo $directory
+for x in {2..4}
+do
+   for y in {0..9}
+   do
+      echo "##############"
+      inputfile=$directory'input'$x'-'$y'.txt'
+      echo Testing $inputfile
+      ./gerarquia $inputfile > output.txt
+      outputfile=$directory'output'$x'-'$y'.txt'
+      diff output.txt $outputfile
+   done
 done
-
-average_time=$((total_time / num_runs))
-echo "Average execution time: $average_time ns"
